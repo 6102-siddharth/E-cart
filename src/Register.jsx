6a688daf84a68df18d74984aff62d259865aa56b
6102-siddharth <siddharth.vadgaonkar06@gmail.com>
2024-12-01 23:25:30 +0530

    Icons added

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,57 +1,55 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-
-const Register =() => {
+import { FaRegUser, FaEnvelope, FaLock } from 'react-icons/fa';
+const Register = () => {
     return (
         <>
-        <div className='body1'>
-            <div className='container1'>
+            <div className='body1'>
+                <div className='container1'>
                     <h1>New Registration</h1>
-                    <hr style={{border:"6px solid white",borderRadius:"50%"}}/>
-                        <div className="radio-class">
-                            <div>
-                                <input type="radio" name="select" className='radio-btn'/>
-                                <label htmlFor="">Personal</label>
-                            </div>
-                            
-                            <div>
-                                <input type="radio" name="select" className='radio-btn'/>
-                                <label htmlFor="">Company</label>
-                            </div>
+                    <hr style={{ border: "6px solid white", borderRadius: "50%" }} />
+                    <div className="radio-class">
+                        <div>
+                            <input type="radio" name="select" className='radio-btn' />
+                            <label htmlFor="">Personal</label>
                         </div>
-                        <hr style={{border:"6px solid white",borderRadius:"50%"}}/>
-                        <div className="reginfo" >
-                            <div className='reg-icons'>
-                                 <div className="App">
-                                     <FontAwesomeIcon icon={faEnvelope} />
-                                </div>
-                                <input type="text" placeholder="Enter Full Name" className="reginfo1"/>
-                            </div>
-                            <div className='reg-icons'>
-                                <div className="App">
-                                     <FontAwesomeIcon icon={faEnvelope} />
-                                </div>
-                                <input type="E-mail" placeholder="Email" className="reginfo1"/>
-                            </div>
-                            <div className='reg-icons'>
-                                 <div className="App">
-                                     <FontAwesomeIcon icon={faEnvelope} />
-                                </div>
-                                <input type="password" placeholder="Password" className="reginfo1" />
-                            </div>
-                            <div className='reg-icons'>
-                                <div className="App">
-                                     <FontAwesomeIcon icon={faEnvelope} />
-                                </div>
-                                <input type="password" placeholder="Confirm Password" className="reginfo1" />
-                            </div>
+
+                        <div>
+                            <input type="radio" name="select" className='radio-btn' />
+                            <label htmlFor="">Company</label>
                         </div>
-                        <div id="register-btn">
-                              <button type="button" className="register-btn">Submit</button>
+                    </div>
+                    <hr style={{ border: "6px solid white", borderRadius: "50%" }} />
+                    <div className="reginfo" >
+                        <div className='reg-icons'>
+                            <div className="App">
+                                <FaRegUser></FaRegUser>
+                            </div>
+                            <input type="text" placeholder="Enter Full Name" className="reginfo1" />
                         </div>
+                        <div className='reg-icons'>
+                            <div className="App">
+                                <FaEnvelope></FaEnvelope>
+                            </div>
+                            <input type="E-mail" placeholder="Email" className="reginfo1" />
+                        </div>
+                        <div className='reg-icons'>
+                            <div className="App">
+                                <FaLock></FaLock>
+                            </div>
+                            <input type="password" placeholder="Password" className="reginfo1" />
+                        </div>
+                        <div className='reg-icons'>
+                            <div className="App">
+                                <FaLock></FaLock>
+                            </div>
+                            <input type="password" placeholder="Confirm Password" className="reginfo1" />
+                        </div>
+                    </div>
+                    <div id="register-btn">
+                        <button type="button" className="register-btn">Submit</button>
+                    </div>
+                </div>
             </div>
-        </div>
         </>
     )
 }
